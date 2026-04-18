4b705b9ec299d26b9d34d3bd5062e0392d92043d
Smit Patel <smitrpatel05@gmail.com>
2026-04-18 13:39:27 +0530

    fixed

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -42,8 +42,12 @@ async function registerUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token)
-
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,        //required for HTTPS (production)
+    sameSite: "none",    // required for cross-origin (Vercel ↔ Render)
+    maxAge: 24 * 60 * 60 * 1000 // optional (1 day)
+})
 
     res.status(201).json({
         message: "User registered successfully",
@@ -88,7 +92,12 @@ async function loginUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,        //required for HTTPS (production)
+    sameSite: "none",    // required for cross-origin (Vercel ↔ Render)
+    maxAge: 24 * 60 * 60 * 1000 // optional (1 day)
+})
     res.status(200).json({
         message: "User loggedIn successfully.",
         user: {
